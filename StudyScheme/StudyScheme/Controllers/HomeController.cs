bd9ee6dcbb6dd99ac3fe136214a5fbd856c801fb
Joel <iflyckt@hotmail.com>
2016-10-24 11:47:02 +0200

    a

--- StudyScheme/StudyScheme/Controllers/HomeController.cs
+++ StudyScheme/StudyScheme/Controllers/HomeController.cs
@@ -15,37 +15,16 @@ namespace StudyScheme.Controllers
 
         public ActionResult About()
         {
-           
+
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            
+
 
             return View();
-        }
-        public ActionResult Create()
-        {
-            ViewBag.Br_Bil = new SelectList(db.Students, "Bil_Id", "Bil_RegNr");
-            return View();
-        }
-
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Br_Id,Br_EfterNamn,Br_ForNamn,Br_PersonNr,Br_Stad,Br_Bil")] Tbl_Brukare tbl_Brukare)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Tbl_Brukare.Add(tbl_Brukare);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            ViewBag.Br_Bil = new SelectList(db.Tbl_Bil, "Bil_Id", "Bil_RegNr", tbl_Brukare.Br_Bil);
-            return View(tbl_Brukare);
         }
     }
 }
