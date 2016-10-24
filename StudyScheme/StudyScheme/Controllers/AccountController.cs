866c3ff50f7b8873f66359c1a8d24a75ba3ff0bc
Joel <iflyckt@hotmail.com>
2016-10-24 15:21:04 +0200

    as

--- StudyScheme/StudyScheme/Controllers/AccountController.cs
+++ StudyScheme/StudyScheme/Controllers/AccountController.cs
@@ -392,7 +392,7 @@ namespace StudyScheme.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Login", "Account");
         }
 
         //
--- StudyScheme/StudyScheme/Models/AccountViewModels.cs
+++ StudyScheme/StudyScheme/Models/AccountViewModels.cs
@@ -65,6 +65,17 @@ namespace StudyScheme.Models
     public class RegisterViewModel
     {
         [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 2)]
+        [Display(Name = "Firstname")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 2)]
+        [Display(Name = "Lastname")]
+        public string LastName { get; set; }
+
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
